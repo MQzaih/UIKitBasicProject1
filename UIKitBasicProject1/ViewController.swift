570550513bcae462887a757a08c1b355da513c34
MQzaih <masaqzaih@gmail.com>
2020-10-29 10:47:25 +0200

    last update for phase 1

--- UIKitBasicProject1/ViewController.swift
+++ UIKitBasicProject1/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
     var myData = ["","second","third"]
     var users = [user] ()
-    var rqm = 0
+    var index = 0
     @IBOutlet var userListTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         users.append(user(fullName: "Gucci2 ",username: "@diamond",phone: 05999,emailAddress: "mqzaih@asaltech.com",website: "www.me.com"))
         users.append(user(fullName: "Gucci3 ",username: "@diamond",phone: 05999,emailAddress: "mqzaih@asaltech.com",website: "www.me.com"))
         users.append(user(fullName: "Gucci4 ",username: "@diamond",phone: 05999,emailAddress: "mqzaih@asaltech.com",website: "www.me.com"))
-        //view.backgroundColor =        // Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return users.count
@@ -54,31 +53,25 @@ return cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         guard indexPath.row < users.count else {
-print("hi")
             return
 
         }
-        rqm = indexPath.row
+        index = indexPath.row
         self.performSegue(withIdentifier: "profile", sender: self)
 
     }
     
  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    print("hsdjhdehd&&&&&&&")
-    print(segue.identifier)
+   
     if segue.identifier == "profile" {
-        print(segue.destination)
         if let vc = segue.destination as? ProfileVCViewController {
-            vc.name = users[rqm].fullName
-            vc.email = users[rqm].emailAddress
-            vc.website = users[rqm].website
-            vc.phone = String(users[rqm].phone)
-            vc.username = users[rqm].username
-        print("in")
+            vc.name = users[index].fullName
+            vc.email = users[index].emailAddress
+            vc.website = users[index].website
+            vc.phone = String(users[index].phone)
+            vc.username = users[index].username
         }
-       else {
-        print ("out ")
-        }
+    
     }
     }
 
